1eda99428ed56befb2771f0b97402426c417b1d5
s5108596 <c.tao@griffith.edu.au>
2020-03-19 11:50:19 +1000

    03191150

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <div className='search-bar ui segment'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
+                    <div className='field'>
+                        <label>Video Search</label>
+                        <input
+                            type='text'
+                            value={this.state.term}
+                            onChange={e => this.setState({term: e.target.value})}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
