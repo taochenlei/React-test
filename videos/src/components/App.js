56b795dfbefd44f1197187f5d5473b3221952e16
s5108596 <c.tao@griffith.edu.au>
2020-03-20 08:55:41 +1000

    video app completed! wah! so cooooool!!!

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+
 import youtube from '../apis/Youtube';
+
 
 
 class App extends React.Component {
     state = {videos: [], selectedVideo: null};
 
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {q: term}
         });
-        this.setState({videos: response.data.items})
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     };
 
     onVideoSelect = (video) => {
@@ -23,9 +32,16 @@ class App extends React.Component {
         return (
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
-                <VideoDetail/>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className='ui grid'>
+                    <div className='ui row'>
+                        <div className='ten wide column'>
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className='six wide column'>
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const VideoDetail = (props) => {
+const VideoDetail = ({video}) => {
+    if (!video) {
+        return <div>Loading...</div>
+    };
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
     return (
         <div>
-            VideoDetail
+            <div className='ui embed'>
+                <iframe title='video player' src={videoSrc} />
+            </div>
+            <div className='ui segment'>
+                <h4 className='title'>{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     );
 };
