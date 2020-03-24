1bab76cc8bcffeb394a9250709e84a76281d2440
Jesse Tao <c.tao@griffith.edu.au>
2020-03-24 11:49:41 +1000

    up tp 223 (Redux with G auth)

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,33 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
-const PageOne = () => {
+const App = () => {
     return (
-        <div>
-            PageOne
-            <Link to='/p2'>Navigate to page two</Link>
+        <div className='ui container'>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' exact component={StreamCreate}/>
+                    <Route path='/streams/edit' exact component={StreamEdit}/>
+                    <Route path='/streams/delete' exact component={StreamDelete}/>
+                    <Route path='/streams/show' exact component={StreamShow}/>
+                </div>
+            </BrowserRouter>
         </div>
-    );
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo
-            <Link to='/'>Navigate to page one</Link>
-        </div>
-    );}
-    
-    const App = () => {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Route path='/' exact component={PageOne} />
-                        <Route path='/p2' exact component={PageTwo} />
-                    </div>
-                </BrowserRouter>
-            </div>
     );
 };
 
