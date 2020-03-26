56469c61e2b7eabb6eb90bb3500d1a5ef7348e84
Jesse Tao <c.tao@griffith.edu.au>
2020-03-27 09:52:07 +1000

    up tp 315 before hook

---
+++ language/src/components/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+    // static contextType = LanguageContext;
+
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit': '提交';
+    }
+
+    renderButton(color) {
+        return(
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {({language}) => this.renderSubmit(language)}
+                </LanguageContext.Consumer>
+            </button>
+        )
+    }
+
+    render() {
+        // const text = this.context === 'english' ? 'Submit' : '提交';
+        return (
+            <ColorContext.Consumer>
+                {(color) =>this.renderButton(color)}
+            </ColorContext.Consumer>
+        );
+    }
+}
+
+export default Button;
