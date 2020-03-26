c7d1811cc0796ae984191271f1e7b4e5500f863e
Jesse Tao <c.tao@griffith.edu.au>
2020-03-26 10:23:07 +1000

    up tp 288 all functionalities done!

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -7,11 +7,11 @@ import StreamForm from "./StreamForm";
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
-    }
+    };
 
     onSubmit = formValues => {
         this.props.editStream(this.props.match.params.id, formValues);
-    }
+    };
 
     render() {
         if (!this.props.stream) {
@@ -27,7 +27,7 @@ class StreamEdit extends React.Component {
             </div>
         )
     }
-};
+}
 
 const mapStateToProps = (state, ownProps) => {
     return {stream: state.streams[ownProps.match.params.id]};
